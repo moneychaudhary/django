76535a95aad50189254976e0c250a273ed7ce02b
moneychaudhary5 <moneychaudhary5@gmail.com>
2018-01-04 23:11:17 +0530

    Added all rest api methods

--- src/testproject/testapp/views.py
+++ src/testproject/testapp/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from . import serializers
+from rest_framework import status
 
 
 # Create your views here.
 class HelloAPIView(APIView):
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
-        return Response({'messages' : 'Hello!'})
+        return Response({'messages': 'Hello!'})
+
+    def post(self, request):
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        return Response({'method': 'put'})
+
+    def patch(self, request, pk=None):
+        return Response({'method': 'patch'})
+
+    def delete(self, request, pk=None):
+        return Response({'method': 'delete'})
